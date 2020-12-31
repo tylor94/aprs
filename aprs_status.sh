02fe491965223b90bc07c7de9f28dd2d50e26584
Tylor <tnewman94@gmail.com>
2020-12-30 20:09:41 -0500

    Updated aprs status script

--- aprs_status.sh
+++ aprs_status.sh
@@ -7,23 +7,26 @@ cputf=$(/opt/vc/bin/vcgencmd measure_temp | awk -F "[=\']" '{print($2 * 1.8)+32}
 # Formatting variables
 padding="echo"
 newline="\n"
+delimiter=" "
 
 printf ">" ;\
 \
 # Time
 printf "$(date +%I):$(date +%M)" ;\
 printf "$(date +%p)" ;\
-printf "/" ;\
+printf "$delimiter" ;\
 #printf " " ;\
 \
 # Battery
 printf "err" ;\
 printf "V" ;\
+printf "$delimiter" ;\
 #printf " " ;\
 \
 # CPU temp
-#printf "$cputc" ;\
-#printf "C" ;\
+printf "$cputc" ;\
+printf "°C" ;\
+#printf "$delimiter" ;\
 #printf " " ;\
 \
 # This newline must be here or Aprx will fail to execute this script.
