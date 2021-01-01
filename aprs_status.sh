41e2da39bf8137e417e946adebaa46eb1d951726
Tylor <tnewman94@gmail.com>
2021-01-01 16:34:06 -0500

    434

--- aprs_status.sh
+++ aprs_status.sh
@@ -3,7 +3,7 @@
 # Script variables
 cputc=$(/opt/vc/bin/vcgencmd measure_temp | cut -d "." -f 1 | cut -d "=" -f 2)
 #cputf=$(/opt/vc/bin/vcgencmd measure_temp | awk -F "[=\']" '{print($2 * 1.8)+32}' | cut -d "." -f 1)
-oneminload=$(cat /proc/loadavg | cut -d " " -f 1)
+load=$(cat /proc/loadavg | cut -d " " -f 3) # 15 min load
 
 # Formatting variables
 padding="echo"
@@ -30,10 +30,10 @@ printf "°C" ;\
 printf "$delimiter" ;\
 #printf " " ;\
 \
-# CPU one minute load
-printf "$oneminload" ;\
+# CPU load
+printf "$load" ;\
 #printf "$delimiter" ;\
 #printf " " ;\
 \
-# This newline must be here or Aprx will fail to execute this script.
+# This newline must be here or APRX will fail to execute this script.
 printf "$newline" ;\
