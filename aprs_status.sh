a2b63f5ad7820a6a50932bf2ba8038a560613401
Tylor <tnewman94@gmail.com>
2021-02-15 22:18:06 -0500

    Added ATRV support

--- aprs_status.sh
+++ aprs_status.sh
@@ -4,6 +4,8 @@
 cputc=$(/opt/vc/bin/vcgencmd measure_temp | cut -d "." -f 1 | cut -d "=" -f 2)
 #cputf=$(/opt/vc/bin/vcgencmd measure_temp | awk -F "[=\']" '{print($2 * 1.8)+32}' | cut -d "." -f 1)
 load=$(cat /proc/loadavg | cut -d " " -f 3) # 15 min load
+atrv=$(cat ./atrv_output.log | cut -d ">" -f 2 | cut -d "V" -f 1)
+vcorr=1
 
 # Formatting variables
 padding="echo"
@@ -15,24 +17,21 @@ printf ">" ;\
 # Time
 printf "@$(date +%d%H%M)" ;\
 printf "$delimiter" ;\
-#printf " " ;\
 \
 # Battery
-#printf "err" ;\
-#printf "*V" ;\
-#printf "$delimiter" ;\
-#printf " " ;\
+#printf "$atrv"
+printf "$(echo $atrv + $vcorr | bc)"
+printf "+V" ;\
+printf "$delimiter" ;\
 \
 # CPU temp
 printf "$cputc" ;\
 printf "°C" ;\
 printf "$delimiter" ;\
-#printf " " ;\
 \
 # CPU load
 printf "$load" ;\
 #printf "$delimiter" ;\
-#printf " " ;\
 \
 # This newline must be here or APRX will fail to execute this script.
 printf "$newline" ;\
